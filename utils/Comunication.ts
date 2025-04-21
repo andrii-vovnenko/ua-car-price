776e165b0b990dd55c7c62273e8456bfa98e4c41
a.vovnenko <vovnenkoandrii@gmail.com>
2025-04-21 16:07:43 +0200

    Add comunication

---
+++ utils/Comunication.ts
@@ -0,0 +1,54 @@
+import { browser } from 'wxt/browser';
+import { RawCarData } from './parseCarData';
+import { DefaultCarEntity } from './types';
+const actions = {
+  INJECT_CONTENT_SCRIPT: 'INJECT_CONTENT_SCRIPT',
+  API_RESPONSE: 'API_RESPONSE',
+  RAW_CAR_DATA: 'RAW_CAR_DATA',
+} as const;
+
+type Action = typeof actions[keyof typeof actions];
+
+type CallbackType<T extends Action> = 
+  T extends typeof actions.INJECT_CONTENT_SCRIPT ? () => void :
+  T extends typeof actions.API_RESPONSE ? (carData: { brand: DefaultCarEntity, model: DefaultCarEntity, fuel: DefaultCarEntity, productionYear: DefaultCarEntity }) => void :
+  T extends typeof actions.RAW_CAR_DATA ? (rawCarData: RawCarData) => void :
+  never;
+
+export class Communication {
+  private static instance: Communication;
+  actions = actions;
+  private browser = browser;
+  private listeners: Record<Action, Function[]> = {
+    [this.actions.INJECT_CONTENT_SCRIPT]: [],
+    [this.actions.API_RESPONSE]: [],
+    [this.actions.RAW_CAR_DATA]: [],
+  };
+
+  private constructor() {
+    this.browser.runtime.onMessage.addListener((message) => {
+      const action = message.action as Action;
+      const callbacks = this.listeners[action];
+      if (callbacks) {
+        callbacks.forEach(callback => callback(message.params));
+      }
+    })
+  }
+
+  public static getInstance(): Communication {
+    if (!Communication.instance) {
+      Communication.instance = new Communication();
+    }
+    return Communication.instance;
+  }
+
+  public listen<T extends Action>(event: T, callback: CallbackType<T>) {
+    this.listeners[event].push(callback);
+  }
+
+  public emit<T extends Action>(event: T, data?: Parameters<CallbackType<T>>[0]): void {
+    this.browser.runtime.sendMessage({ action: event, params: data });
+  }
+}
+
+export const communication = Communication.getInstance();
